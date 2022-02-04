2de49ff206582255a32904f1d1dcd3490fe4b050
yeeter030 <2002234i@tp.edu.sg>
2022-02-04 14:09:30 +0800

    GameApp With Error

--- Worksheet5/Assets/Scripts/GameApp.cs
+++ Worksheet5/Assets/Scripts/GameApp.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PGGE.Patterns;
+using UnityEngine.SceneManagement;
 
-public class GameApp : MonoBehaviour
+public class GameApp : Singleton<GameApp>
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        GamePaused = false;
     }
 
-    // Update is called once per frame
+    public bool GamePaused
+    {
+        get { return mPause; }
+        set
+        {
+            mPause = value;
+            mOnPause?.Invoke(GamePaused);
+            if (GamePaused)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GamePaused = !GamePaused;
+        }
     }
+
+    // called first
+    void OnEnable()
+    {
+        Debug.Log("OnEnable called");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // called when the game terminates
+    void OnDisable()
+    {
+        Debug.Log("OnDisable");
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded - Scene Index: " + scene.buildIndex + " Scene Name: " + scene.name);
+        Debug.Log(mode);
+    }
+
 }
+
